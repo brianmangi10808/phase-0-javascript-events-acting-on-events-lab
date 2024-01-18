29c43d7f513b1c4bc6b320f6496953d51f95fbf0
Your Name <brianmwangi10808@gmail.com>
2024-01-18 16:09:37 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+// Your implementation of moveDodgerLeft and moveDodgerRight functions
+function moveDodgerLeft() {
+   
+}
+
+function moveDodgerRight() {
+    
+}
+
+
+describe('moveDodgerLeft()', () => {
+    let dodger;
+
+    beforeEach(() => {
+       
+        dodger = document.getElementById('dodger');
+    });
+
+    it('moves the DODGER to the left', () => {
+        const left = parseInt(dodger.style.left) || 0;
+
+        moveDodgerLeft();
+
+        const newPosition = parseInt(dodger.style.left) || 0;
+
+        expect(newPosition).to.be.below(left);
+    });
+});
+
+
+describe('moveDodgerRight()', () => {
+    let dodger;
+
+    beforeEach(() => {
+        
+        dodger = document.getElementById('dodger');
+    });
+
+    it('moves the DODGER to the right', () => {
+        const left = parseInt(dodger.style.left) || 0;
+
+        moveDodgerRight();
+
+        const newPosition = parseInt(dodger.style.left) || 0;
+
+        expect(newPosition).to.be.above(left);
+    });
+});
